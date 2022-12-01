7f9ea0c366acb30d29523de1bde39e2a01131a52
Roman Princ <u19.421@pslib.cloud>
2022-12-01 11:32:05 +0100

    init

--- binary_tree/Program.cs
+++ binary_tree/Program.cs
@@ -1,86 +1,147 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//create a new tree
-var tree = new BinaryTree();
+//Binary tree
+//DU - 1) Count jako vlastnost ktera se bude pocitat
+//	   2) Genericky binary tree
+
+var newtree = new BinaryTree();
+newtree.Insert(5);
+newtree.Insert(1);
+newtree.Insert(7);
+newtree.Insert(2);
+newtree.Insert(4);
+Console.WriteLine(newtree.ToString());
 
 public class BinaryTree
 {
-    public Node Root { get; set; }
-
+	Node root;
+    string output = "";
     public BinaryTree()
-    {
-        Root = null;
+	{
+        root = null;
+	}
+
+	public void Insert(int input)
+	{
+		
+		if (root == null)
+		{
+            root = new Node(input);
+			return;
+		}
+		InsertRecursively(root, input);
+
     }
 
-    public void Add(int value)
-    {
-        var node = new Node(value);
-        if (Root == null)
-        {
-            Root = node;
+	void InsertRecursively(Node root, int newnode)
+	{
+		if (newnode <= root.value)
+		{
+			if (root.left == null)
+			{
+				root.left = new Node(newnode);
+			}
+			else
+			{
+                InsertRecursively(root.left, newnode);
+            }
         }
+		else
+		{
+            
+            if (root.right == null)
+            {
+                root.right = new Node(newnode);
+            }
+			else
+			{
+                InsertRecursively(root.right, newnode);
+            }
+            
+            
+        }
+	}
+
+	public bool Delete(int input)
+	{
+		if (root == null)
+		{
+			Console.WriteLine("No elems");
+		}
+		return DeleteRecursively(root, input);
+	}
+
+	bool DeleteRecursively(Node root, int input)
+	{
+		if (root.value == input)
+		{
+			// 1) nemame zadneho naslednika, u predchudce zmenime na null
+			// 2) mam jednoho naslednika, u predchudce nahradim sebe naslednikem
+			// 3) mam oba nasledniky, upredchudce nahradim sebe jednim naslednikem a podstrom druheho naslednika, zaradim do prvniho naslednika
+		}
+		else if (input <= root.value)
+		{
+			if (root.left != null)
+			{
+				return DeleteRecursively(root.left, input);
+			}
+			return false;
+		}
         else
         {
-            Root.Add(node);
+            if (root.right != null)
+            {
+                return DeleteRecursively(root.right, input);
+            }
+            return false;
         }
     }
 
-    public void Print()
-    {
-        Root.Print();
+
+
+	public override string ToString()
+	{
+		
+		if (root != null)
+		{
+			PrintRecursively(root);
+		}
+		return output;
+	}
+
+
+	void PrintRecursively(Node root)
+	{
+		if (root.left != null)
+		{
+			PrintRecursively(root.left);
+			
+		}
+		output = output + ", " + root.value;
+
+		if (root.right != null)
+		{
+			PrintRecursively(root.right);
+			
+		}
+
     }
 }
 
 public class Node
 {
-    
-    public int Value { get; set; }
-    public Node Left { get; set; }
-    public Node Right { get; set; }
+	public int value;
+    public Node left;
+	public Node right;
 
-    public Node(int value)
-    {
-        Value = value;
-    }
-
-    public void Add(Node node)
-    {
-        if (node.Value < Value)
-        {
-            if (Left == null)
-            {
-                Left = node;
-            }
-            else
-            {
-                Left.Add(node);
-            }
-        }
-        else
-        {
-            if (Right == null)
-            {
-                Right = node;
-            }
-            else
-            {
-                Right.Add(node);
-            }
-        }
-    }
-
-    public void Print()
-    {
-        if (Left != null)
-        {
-            Left.Print();
-        }
-        Console.WriteLine(Value);
-        if (Right != null)
-        {
-            Right.Print();
-        }
-    }
+	public Node(int val)
+	{
+		value = val;
+		left = right = null;
+		
+	}
 }
-
